22669035e27a7ef7778eef959b843d0f39fd8a62
himaniagarwal04 <himgrwal04@gmail.com>
2024-10-15 12:31:59 +0530

    Fixing build.gradle.kts files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,32 @@
-plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-}
-
-android {
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.currencyexchangerates"
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-    compileSdk = 34
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-}
-
-dependencies {
-    implementation(platform(libs.androidx.core.bom)) // Assuming 'bom' is defined in your libs object
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.constraintlayout)
-}
+//plugins {
+//    alias(libs.plugins.android.application)
+//    alias(libs.plugins.kotlin.android)
+//}
+//
+//android {
+//    compileSdk = 34
+//
+//    defaultConfig {
+//        applicationId = "com.example.currencyexchangerates"
+//        minSdk = 21
+//        targetSdk = 34
+//        versionCode = 1
+//        versionName = "1.0"
+//
+//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+//    }
+//    compileSdk = 34
+//
+//    buildTypes {
+//        release {
+//            isMinifyEnabled = false
+//            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+//        }
+//    }
+//}
+//
+//dependencies {
+//    implementation(platform(libs.androidx.core.bom)) // Assuming 'bom' is defined in your libs object
+//    implementation(libs.androidx.appcompat)
+//    implementation(libs.androidx.constraintlayout)
+//}
